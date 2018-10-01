b612fdec85ea4cff5cd9a6c964785744404fa938
Pjurk <patryk.jurkiewicz19@gmail.com>
2018-10-01 17:41:51 +0000

    Second

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+var friends = ["Tony", "Miranda", "Jasmin"];
+
+app.post("/addFriend", function(req, res){
+    var newFriend = req.body.name;
+    friends.push(newFriend);
+    res.redirect("/friend");
+})
+
 app.get("/", function(req, res){
-    res.send("Home");
+    res.render("home");
+})
+
+app.get("/friend", function(req,res){   
+    res.render("friend", {friends: friends});
 })
 
 app.listen(3000, function(){
